e92f350467ee2467ab753add729778fa3470dc8d
xenor <xenor@lifefight.de>
2010-11-07 19:23:28 +0100

    some fixes

--- Main.cs
+++ Main.cs
@@ -8,49 +8,61 @@ namespace gamesrv
 {
 	class MainClass
 	{
-		public static string version;
 		
+		public static string version;
 		public static TcpListener tcpListener;
 		public static Thread listenThread;
-		
-		public static Array users;
+		public static UTF8Encoding encoder = new UTF8Encoding();
+		public static object[] users = new object[4096];
+		public static int user_count = 0;
 		
 		private static void HandleClientComm(object client)
 		{
 			TcpClient tcpClient = (TcpClient)client;
 			NetworkStream clientStream = tcpClient.GetStream();
+			users[user_count] = client;
+			user_count++;
+			
+			byte[] buffer = new byte[128];
+			buffer = encoder.GetBytes(user_count.ToString());
+			clientStream.Write(buffer, 0, buffer.Length);
 			
 			byte[] message = new byte[4096];
 			int bytesRead;
 			
 			while (true)
 			{
-			bytesRead = 0;
+				#region shit
+				bytesRead = 0;
+				
+				try
+				{
+					//blocks until a client sends a message
+					bytesRead = clientStream.Read(message, 0, 4096);
+				}
+				catch
+				{
+					//a socket error has occured
+					break;
+				}
+				
+				if (bytesRead == 0)
+				{
+					//the client has disconnected from the server
+					break;
+				}
 			
-			try
-			{
-				//blocks until a client sends a message
-				bytesRead = clientStream.Read(message, 0, 4096);
-			}
-			catch
-			{
-				//a socket error has occured
-				break;
-			}
-			
-			if (bytesRead == 0)
-			{
-				//the client has disconnected from the server
-				break;
-			}
-			
-			//message has successfully been received
-			ASCIIEncoding encoder = new ASCIIEncoding();
-			Console.WriteLine(encoder.GetString(message, 0, bytesRead).Trim());
+				//message has successfully been received
+				string str = encoder.GetString(message, 0, bytesRead).Trim();
+				Console.WriteLine("incoming string from " + client.ToString() + ": " + str);
+				#endregion
+				
 			}
 			
 			tcpClient.Close();
 		}
+		
+		#region acceptshit
 		
 		private static void ListenForClients()
 		{
@@ -74,6 +86,8 @@ namespace gamesrv
 			listenThread = new Thread(new ThreadStart(ListenForClients));
 			listenThread.Start();
 		}
+		
+		#endregion
 	}
 }
 
