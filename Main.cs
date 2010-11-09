0be044781e23af6be639b3311fd066a0a3a778da
xenor <xenor@lifefight.de>
2010-11-09 18:37:02 +0100

    ping/pong added

--- Main.cs
+++ Main.cs
@@ -4,6 +4,7 @@ using System.Net.Sockets;
 using System.Threading;
 using System.Net;
 using System.Data;
+using System.Collections.Generic;
 using MySql.Data.MySqlClient;
 
 namespace gamesrv
@@ -14,12 +15,45 @@ namespace gamesrv
         public int user_id;
         public int logintime;
         public NetworkStream stream;
+        public int lastpong = gamesrv.MainClass.unixtime();
         public user(int user_id, NetworkStream stream)
         {
             this.user_id = user_id;
             this.stream = stream;
-            long time = DateTime.Now.ToFileTime();
-            Console.WriteLine("[ NEWUSER ] [ " + user_id + " ]: TIME: " + time);
+            int time = gamesrv.MainClass.unixtime();
+            gamesrv.MainClass.say("[ NEWUSER ] [ " + user_id + " ]: TIME: " + time);
+        }
+    }
+
+    public class pingbot
+    {
+        public static void start()
+        {
+            while (true)
+            {
+                System.Threading.Thread.Sleep(config.pingtimeout);
+                int curtime = gamesrv.MainClass.unixtime();
+                try
+                {
+                    for (int i = 0; i < gamesrv.MainClass.allusers.Count; i++ )
+                    {
+                        user thisuser = gamesrv.MainClass.allusers[i];
+                        if (thisuser != null && thisuser.stream != null)
+                        {
+                            int timespan = (config.pingtimeout / 1000) - (curtime - thisuser.lastpong);
+                            if (timespan < 0)
+                            {
+                                gamesrv.MainClass.closeConn(thisuser);
+                            }
+                            else
+                            {
+                                gamesrv.MainClass.writeToStream(thisuser.stream, "PING");
+                            }
+                        }
+                    }
+                }
+                catch { }
+            }
         }
     }
 
@@ -27,13 +61,24 @@ namespace gamesrv
     {
         public class mysql
         {
-            public static string config = "SERVER=localhost;DATABASE=lunatic_3;UID=lunatic3;PASSWORD=OPFER;";
+            public const string config = "SERVER=localhost;DATABASE=lunatic_3;UID=lunatic3;PASSWORD=OPFER;";
         }
         public class locale
         {
-            public static string welcome = "WELCOME";
-            public static string bye = "BYE";
+            public const string welcome = "WELCOME";
+            public const string bye = "BYE";
         }
+        public class info
+        {
+            public static int major_version = 0;
+            public static int minor_version = 1;
+            public static int sub_version = 5;
+            public static string version = major_version + "."
+                                                + minor_version + "."
+                                                + sub_version;
+            public static string[] masternames = { "Anohros", "xenor" };
+        }
+        public static int pingtimeout = 10000;
     }
 
     class MainClass
@@ -47,7 +92,22 @@ namespace gamesrv
 
         public static NetworkStream[] users = new NetworkStream[4096];
         public static int user_count = 0;
-        public static user[] allusers = new user[16];
+        //public static user[] allusers = new user[16];
+        public static List<user> allusers = new List<user>();
+
+        public static int unixtime()
+        {
+            DateTime date1 = new DateTime(1970, 1, 1);
+            DateTime date2 = DateTime.Now;
+            TimeSpan ts = new TimeSpan(date2.Ticks - date1.Ticks);
+            return (Convert.ToInt32(ts.TotalSeconds));
+        }
+
+        public static void closeConn(user user)
+        {
+            gamesrv.MainClass.allusers.Remove(user);
+            user.stream.Close();
+        }
 
         public static user findUserByStream(NetworkStream stream)
         {
@@ -71,7 +131,7 @@ namespace gamesrv
                 try
                 {
                     stream.Write(buffer, 0, buffer.Length);
-                    Console.WriteLine("[   >>>   ] [ " + thisuser.user_id + " ]: " + text);
+                    say("[   >>>   ] [ " + thisuser.user_id + " ]: " + text);
                 }
                 catch
                 {
@@ -87,7 +147,7 @@ namespace gamesrv
             users[user_count] = clientStream;
             user_count++;
 
-            allusers[user_count] = new user(0, clientStream);
+            allusers.Add(new user(0, clientStream));
 
             writeToStream(clientStream, config.locale.welcome);
 
@@ -118,7 +178,7 @@ namespace gamesrv
                 //message has successfully been received
                 string str = encoder.GetString(message, 0, bytesRead).Trim();
                 user thisuser = findUserByStream(clientStream);
-                Console.WriteLine("[   <<<   ] [ " + thisuser.user_id + " ]: " + str);
+                say("[   <<<   ] [ " + thisuser.user_id + " ]: " + str);
                 string[] cmd = str.Split(' ');
                 cmd[0] = cmd[0].ToUpper();
                 if (cmd[0] == "QUIT")
@@ -129,7 +189,7 @@ namespace gamesrv
                 else if (cmd[0] == "NOTICE")
                 {
                     string str2 = str.Substring(7);
-                    Console.WriteLine("[  SPRED  ] [ " + thisuser.user_id + " ]: " + str2);
+                    say("[  SPRED  ] [ " + thisuser.user_id + " ]: " + str2);
                     foreach (user user in allusers)
                     {
                         if (user != null && user.stream != null)
@@ -138,12 +198,22 @@ namespace gamesrv
                         }
                     }
                 }
+                else if (cmd[0] == "PONG")
+                {
+                    thisuser.lastpong = unixtime();
+                }
             }
 
             tcpClient.Close();
         }
 
         #region acceptshit
+
+        public static void say(string str)
+        {
+            string curtime = DateTime.Now.ToString();
+            Console.WriteLine("[ " + curtime + " ]: " + str);
+        }
 
         public static void ListenForClients()
         {
@@ -159,11 +229,13 @@ namespace gamesrv
 
         public static void Main(string[] args)
         {
-            version = "0.1.1";
+            version = config.info.version;
             Console.WriteLine("Game Server v" + version + " starting up...");
             tcpListener = new TcpListener(IPAddress.Any, 3000);
             listenThread = new Thread(new ThreadStart(ListenForClients));
             listenThread.Start();
+            Thread ping_thread = new Thread(new System.Threading.ThreadStart(gamesrv.pingbot.start));
+            ping_thread.Start();
         }
 
         #endregion
